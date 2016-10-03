fe33f809815ddb4d681483bf6e415e4dc9564726
pushkertiwari <pushker@revinfotech.com>
2016-10-03 15:02:34 +0530

    table  creates

---
+++ database/migrations/2016_10_03_085408_create_table_setting.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableSetting extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_setting', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('table_setting_key');
+            $table->text('table_setting_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_setting');
+    }
+}
